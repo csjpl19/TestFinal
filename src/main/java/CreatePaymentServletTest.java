180e10a48beef618df612069758a10a70f7bc586
Pierre Louis Carl Stephen Junior <150768817+csjpl19@users.noreply.github.com>
2025-11-09 22:06:18 -0500

    Remove CLIENT_SECRET from CreatePaymentServletTest
    Removed sensitive CLIENT_SECRET value from test class.

--- src/main/java/CreatePaymentServletTest.java
+++ src/main/java/CreatePaymentServletTest.java
@@ -18,7 +18,7 @@ public class CreatePaymentServletTest extends HttpServlet {
 
     // --- VOS INFORMATIONS MONCASH (TEST) ---
     private static final String CLIENT_ID = "";
-    private static final String CLIENT_SECRET = "JucuikGdv2-uwLx8KoTlDeSY2oT1ScsW54AWcb5sOmND-K9kHR1MGr_Jdb-Li36u";
+    private static final String CLIENT_SECRET = "";
     private static final String API_BASE_URL = "https://sandbox.moncashbutton.digicelgroup.com/Api";
     private static final String GATEWAY_BASE_URL = "https://sandbox.moncashbutton.digicelgroup.com/Moncash-middleware";
 
